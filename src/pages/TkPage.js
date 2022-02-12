858f64b01f156fd6c6144ca5c34d261ea1327899
SatakeYusuke19920527 <satkyusk@icloud.com>
2022-02-12 20:56:46 +0900

    commit

--- src/pages/TkPage.js
+++ src/pages/TkPage.js
@@ -1,8 +1,37 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { Link } from 'react-router-dom'
 
 const TkPage = () => {
+  const [text, setText] = useState('')
+  const [text1, setText1] = useState('')
+  const [text2, setText2] = useState('')
+  const [error, setError] = useState('')
+
+  const handleChange = (e) => {
+    setText(() => e.target.value)
+  }
+
+  const hyouji = () => {
+    console.log(text)
+    console.log(error)
+    if (text === "") {
+      setError('何か文字を入力してください')
+      setText1('')
+    } else {
+      setText1(text)
+      setText2('http://birthday-color.cafein.jp/html/'+text+'.html')
+      setError('')
+      console.log(text2)
+    }
+  }
+
+
   return (
+    <div>
     <div>TkPage</div>
+    <p><input type="text" value={text} onChange={handleChange} />
+    <button onClick={hyouji}><Link to={text2}>調べる</Link></button></p>
+    </div>
   )
 }
 
