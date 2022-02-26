5a8570631f88dac778a30e546a71a39b4bbeb485
SatakeYusuke19920527 <satkyusk@icloud.com>
2022-02-26 20:53:39 +0900

    a

--- src/pages/UtPage.js
+++ src/pages/UtPage.js
@@ -1,11 +1,36 @@
 import React from 'react'
+import "../styles/UtPage.css"
+import { useState }from 'react';
+
+
 
 const UtPage = () => {
+  const [text,setText] = useState('')
+  const [message,setMessage] = useState('')
+  const [error, setError] = useState('')
+  const handleText = (str) => {
+    setText(str)
+  }
+
+  const hyoutann = () => {
+    if (text === ""){
+      setMessage(text)
+      setError("文字をenterせよ")
+    }else{
+      setMessage(text)
+      setError("")
+    }}
+
   return (
-    <div>
-      <h1 className="april-a">tuna(title)</h1>
-    </div>
+    <div className="a">
+      <p className="april">tuna(title)</p>
+  <label>にゅ</label>
+  <input type="text" onChange={(e)=>handleText(e.target.value)} />
+  <button onClick={hyoutann} type="button" value="Submit">btton</button>
+  <div style={{color : "red"}}>{error}</div>
+  {message}
+  </div>
   )
-}
+  }
 
 export default UtPage
