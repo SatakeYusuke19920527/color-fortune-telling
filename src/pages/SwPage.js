8ac163a4caa5a2d42c8d90effd012ab0bdaa573d
SatakeYusuke19920527 <satkyusk@icloud.com>
2022-04-02 21:00:02 +0900

    202242

--- src/pages/SwPage.js
+++ src/pages/SwPage.js
@@ -1,13 +1,13 @@
 import React, {useState,useEffect} from 'react'
 import "../styles/SwPage.css"
-
+import { Link } from "react-router-dom";
 
 const SwPage = () => {
   const [siraberu, setSiraberu] = useState('');
   const [siraberuu, setSiraberuu] = useState('');
   const [text, setText] = useState('');
   const [text1, setText1] = useState('');
-  const [sira, setSira] = useState('');
+  // const [sira, setSira] = useState('');
   
  const handleChange = (e) => {
     setSiraberu(() => e.target.value)
@@ -17,9 +17,11 @@ const SwPage = () => {
  setSiraberuu(() => e.target.value)
  }
   
-//    const sira = () => {
-   
-//  }
+  const sira = () => {
+    window.location.replace('http://birthday-color.cafein.jp/' + 'html/' + siraberu + siraberuu + '.html')
+ }
+  
+// 一桁でも出来るようにする
   
   return (
     　<div className="aa">
@@ -28,7 +30,7 @@ const SwPage = () => {
       <div className="aaaa">
       <p>〜月〜<input onChange={handleChange} type="text" value={siraberu} /></p>
       <p>〜日〜<input onChange={handleChange1} type="text1" value={siraberuu} /></p>
-      <br/><button onClick="sira">牡丹</button>
+        <br /><button onClick={sira}>牡丹</button>
     </div>
       </div>
   );
