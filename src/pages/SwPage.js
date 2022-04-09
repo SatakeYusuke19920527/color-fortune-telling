5e65f60da9d2582ddd04c555157e12be2f956dab
SatakeYusuke19920527 <satkyusk@icloud.com>
2022-04-09 20:56:20 +0900

    202049

--- src/pages/SwPage.js
+++ src/pages/SwPage.js
@@ -8,9 +8,12 @@ const SwPage = () => {
   const [text, setText] = useState('');
   const [text1, setText1] = useState('');
   // const [sira, setSira] = useState('');
+  const [sirabeta, setSirabeta] = useState('');
+   const [tiisai, setTiisai] = useState('');
   
  const handleChange = (e) => {
-    setSiraberu(() => e.target.value)
+   setSiraberu(() => e.target.value)
+
  }
    
   const handleChange1 = (e) => {
@@ -18,8 +21,37 @@ const SwPage = () => {
  }
   
   const sira = () => {
-    window.location.replace('http://birthday-color.cafein.jp/' + 'html/' + siraberu + siraberuu + '.html')
- }
+    // window.location.replace('http://birthday-color.cafein.jp/' + 'html/' + sirabeta + siraberuu + '.html')
+//       if (siraberu < 10) {
+//        console.log("numは10より小さいです。");
+//         window.location.replace('http://birthday-color.cafein.jp/' + 'html/' + '0' + siraberu + siraberuu + '.html')
+// }else if (siraberu > 9) {
+//         console.log("numは10以上です。");
+//   window.location.replace('http://birthday-color.cafein.jp/' + 'html/' + siraberu + siraberuu + '.html')
+// }   else if (siraberuu < 10) {
+//         console.log("numは10以上です。");
+//   window.location.replace('http://birthday-color.cafein.jp/' + 'html/' + siraberu + '0' + siraberuu + '.html')
+// }  else if (siraberu > 9) {
+//         console.log("numは10以上です。");
+//   window.location.replace('http://birthday-color.cafein.jp/' + 'html/' + siraberu + siraberuu + '.html')
+//       }
+       if (siraberu < 10, siraberuu < 10) {
+        console.log("numは10以上です。");
+         window.location.replace('http://birthday-color.cafein.jp/' + 'html/' + '0' + siraberu + '0' + siraberuu + '.html')
+}     else if (siraberu < 10,siraberuu > 9) {
+        console.log("numは10以上です。");
+  window.location.replace('http://birthday-color.cafein.jp/' + 'html/' + '0' + siraberu + siraberuu + '.html')
+}   else if (siraberu > 9,siraberuu < 10) {
+        console.log("numは10以上です。");
+  window.location.replace('http://birthday-color.cafein.jp/' + 'html/' + siraberu + '0' + siraberuu + '.html')
+}   else if (siraberu > 9,siraberuu > 9) {
+        console.log("numは10以上です。");
+  window.location.replace('http://birthday-color.cafein.jp/' + 'html/' + siraberu + siraberuu + '.html')
+}   
+ 
+    
+  }
+  
   
 // 一桁でも出来るようにする
   
